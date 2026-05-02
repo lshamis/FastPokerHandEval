878d5b67ac1da40efb427a4beffb94f0f7eeb31a
Leonid Shamis <leonid.shamis@gmail.com>
2026-05-01 21:12:45 -0700

    Add sweep

--- poker_hand_eval.h
+++ poker_hand_eval.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
 #include <cstdint>
 #include <fstream>
 #include <iterator>
@@ -28,6 +29,9 @@ class PokerHandEval {
 
   template <typename Container>
   uint32_t eval(const Container& hand) const;
+
+  template <typename Container, typename Fn>
+  void sweep(const Container& prefix, Fn fn) const;
 
  private:
   std::vector<uint32_t> table_;
@@ -97,4 +101,69 @@ template <uint8_t hand_size>
 template <typename Container>
 uint32_t PokerHandEval<hand_size>::eval(const Container& hand) const {
   return details::EvalHelper<hand_size>::eval_iterator(table_, std::begin(hand));
+}
+
+template <uint8_t hand_size>
+template <typename Container, typename Fn>
+void PokerHandEval<hand_size>::sweep(const Container& prefix, Fn fn) const {
+  uint32_t stack[hand_size + 1] = {};
+  std::array<uint32_t, hand_size> hand;
+
+  // Populate with prefix cards.
+  uint32_t seen_cards[52] = {};
+  for (uint32_t i = 0; i < prefix.size(); i++) {
+    hand[i] = prefix[i];
+    stack[i + 1] = table_[stack[i] + hand[i]];
+
+    seen_cards[hand[i]] = true;
+  }
+
+  // Build deck with remaining cards.
+  uint32_t deck_size = 52 - prefix.size();
+  uint32_t deck[deck_size];
+  uint32_t deck_lookup[52];
+  for (uint32_t di = 0, c = 0; c < 52; c++) {
+    if (!seen_cards[c]) {
+      deck[di] = c;
+      deck_lookup[c] = di;
+      di++;
+    }
+  }
+
+  // Create first legal hand.
+  for (uint32_t hand_idx = prefix.size(); hand_idx < hand_size; hand_idx++) {
+    hand[hand_idx] = deck[hand_idx - prefix.size()];
+    stack[hand_idx + 1] = table_[stack[hand_idx] + hand[hand_idx]];
+  }
+  fn(hand, stack[hand_size]);
+
+  // Generate all remaining hands.
+  while (true) {
+    uint32_t start_idx = hand_size - 1;
+    while (start_idx >= prefix.size()) {
+      uint32_t deck_idx = deck_lookup[hand[start_idx]];
+      uint32_t max_deck_idx = deck_size - (hand_size - start_idx);
+
+      if (deck_idx < max_deck_idx) {
+        // Found the rightmost position that can be incremented.
+        break;
+      }
+      if (start_idx == prefix.size()) {
+        // Cannot advance any further (last combination).
+        return;
+      }
+      start_idx--;
+    }
+    if (start_idx < prefix.size()) {
+      return;
+    }
+
+    // Advance the pivot and refill the tail with the smallest possible cards.
+    uint32_t deck_idx = deck_lookup[hand[start_idx]] + 1;
+    for (uint32_t hand_idx = start_idx; hand_idx < hand_size; hand_idx++, deck_idx++) {
+      hand[hand_idx] = deck[deck_idx];
+      stack[hand_idx + 1] = table_[stack[hand_idx] + hand[hand_idx]];
+    }
+    fn(hand, stack[hand_size]);
+  }
 }
